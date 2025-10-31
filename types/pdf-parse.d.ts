7c6097558cd57c92236e65e73e51fded98fba9d7
tdaniel1925 <trenttdaniel@gmail.com>
2025-10-31 09:49:38 -0500

    Add TypeScript type declarations for pdf-parse module

---
+++ types/pdf-parse.d.ts
@@ -0,0 +1,23 @@
+declare module 'pdf-parse' {
+  interface PDFData {
+    numpages: number;
+    numrender: number;
+    info: any;
+    metadata: any;
+    text: string;
+    version: string;
+  }
+
+  interface PDFOptions {
+    pagerender?: (pageData: any) => string;
+    max?: number;
+  }
+
+  function PDFParse(
+    dataBuffer: Buffer,
+    options?: PDFOptions
+  ): Promise<PDFData>;
+
+  export = PDFParse;
+}
+
